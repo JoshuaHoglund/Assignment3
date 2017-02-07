d2c4a99bd6dd765eb9101fffd9541e3a2439459e
naddl92 <nadja.herbst@icloud.com>
2017-02-07 11:45:41 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,9 +4,14 @@
 #include "file_operations.h"
 
  
-int main(int argc, const char* argv[]) {
+int main(int argc, char** argv[]) { 
+ // read in N filename nsteps delta_t graphics
+ // N number of stars/particles to simulate 
+ // filename is the filename of the file to read the initial configuration from 
+ // nsteps is the number of timesteps
+ // graphics is 1 or 0 meaning graphics on/off
+printf("The program runs." );
 
-  Lukas change
   return 0;
- this is my change/Jo0000shua
+ 
 }
