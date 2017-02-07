88ced30e77907f8dd73709e635333326a3d1d36a
naddl92 <nadja.herbst@icloud.com>
2017-02-07 12:05:36 +0100

    Update main.c

--- main.c
+++ main.c
@@ -11,10 +11,14 @@ int main(int argc, const char* argv[]) {
  // nsteps is the number of timesteps
  // graphics is 1 or 0 meaning graphics on/off
 
+	
+// check if the parameters in the command line are correct, otherwise error message with instructions.	
 	if(argc != 6) {
     printf("Please give in: N filename nsteps delta_t graphics.\n");
     return -1;
   }
+	
+// read in N, check if N is 1 or larger otherwise error message.
 	int N = atoi(argv[1]);
   printf("N = %d\n", N);
   if(N < 1) {
@@ -22,22 +26,28 @@ int main(int argc, const char* argv[]) {
     return -1;
   }
 	
+// read in filename and open filename. 	
  FILE *ptr_file;
 	char buf[1000];
-	
-	
-
 	ptr_file = fopen(argv[2], "r");
-
 	if(!ptr_file){
 		printf("File does not exist." );
 		return 1;}
-	
 	while(fgets(buf, 1000, ptr_file)!=NULL)
 			printf("%s", buf);
-
-			
 	fclose(ptr_file);
+// read in number of steps
+	int nsteps = atoi(argv[3]);
+	printf ("nsteps = %d\n", nsteps);
+	
+	
+	
+// read in time delta t 
+	float delta_t = atoi(argv[4]);
+	printf ("delta_t = %f\n", delta_t);
+	
+	
+// read in graphics turned on 1 or turned off 0
 
   return 0;
  
