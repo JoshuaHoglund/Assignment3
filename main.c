55d8a4f4fbd3178f1a82d5f5f2053e3eb23c25a5
oooooog <cigamaster@gmail.com>
2017-02-09 14:35:33 +0100

    Update main.c

--- main.c
+++ main.c
@@ -166,7 +166,7 @@ int main(int argc, const char* argv[]) {
               //printf("%lf\n", x);
               y = particles[i].y_pos;
               //printf("%lf\n", y);
-              circleRadius = 0.04;
+              circleRadius = 0.006;
               keep_within_box((float*) &x, (float*) &y);
               
               DrawCircle(x, y, L, W, circleRadius, 0.1);
